a6d86b9fc91f84f3f149b0e7447d58c316f5e06c
7gano <n.nagano@gmail.com>
2013-04-03 22:41:19 +0900

    Add Xcode project

---
+++ CF/CF/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  CF
+//
+//  Created by 7gano on 4/3/13.
+//  Copyright (c) 2013 7gano. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
---
+++ CF/CFArray/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CFArray
+//
+//  Created by 7gano on 4/3/13.
+//  Copyright (c) 2013 7gano. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+    CFShow(CFSTR("CFArray"));
+    
+    return 0;
+}
